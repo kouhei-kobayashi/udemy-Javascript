f346a46ff92aadb80003aaf50d51a1c22f5f8f68
kouhei-kobayashi <kouheimac3@gmail.com>
2020-05-11 22:24:22 +0900

    コールバック関数についてその２

--- lesson57/lesson61.js
+++ lesson57/lesson61.js
@@ -1,12 +1,25 @@
 //コールバック関数について
-function hello(name){
-    console.log('hello' + name())
-}
-function getName(){
-    return '航平';
-}
+// function hello(name){
+//     console.log('hello' + name())
+// }
+// function getName(){
+//     return '航平';
+// }
 
-hello(getName);
+// hello(getName);
 //上の例で行くとhello(name) の引数にgetName関数が渡されてconsole.logのnameにreturnで返ってきた航平が渡されている
 //関数を実行するというのは関数の後に()をつけてあげると実行したとみなされる
 
+
+function doSomething(a,b,callback){
+    const result = callback(a,b)
+    console.log(result);
+}
+
+function multiply(a,b){
+    return a * b;
+}
+function callback(){
+
+}
+doSomething(2*2,multiply);
