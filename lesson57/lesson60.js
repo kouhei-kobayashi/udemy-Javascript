648ae0625a507b1868ad7d3171fec8785167c6a6
kouhei-kobayashi <kouheimac3@gmail.com>
2020-05-11 20:48:24 +0900

    アロー関数の書き方

--- lesson57/lesson60.js
+++ lesson57/lesson60.js
@@ -1,5 +1,16 @@
 //アロー関数の使い方
-function hello(name){
-    console.log('hello' + name);
-}
-hello('Code mafia');
+//通常の書き方
+// const hello  = function(name = 'Tom'){
+//     console.log('hello' + name);
+// }
+// function hello(name){
+//     console.log('hello' + name);
+// }
+//hello('Code mafia');
+//アロー関数の書き方
+//アロー関数のメリット
+//コードが簡略化して見やす＝バグが少なくなる
+//引数がひとつなら()はいらない
+const hello = (name,age) => console.log('hello' + name +age);
+
+hello('Code Mafia',20);
