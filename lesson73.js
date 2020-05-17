e67bd2bc21ae02b6898768730cc27bca7eb76c34
kouhei-kobayashi <kouheimac3@gmail.com>
2020-05-17 10:32:57 +0900

    lesson73

---
+++ lesson73.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoded',function(){
+    const btn = document.querySelector('#btn');
+    const ta = new TextAnimation('.animate-title');
+    const ta2 = new TextAnimation('.animate-title-2');
+    ta.animate();
+    ta2.animate();
+    btn.addEventListener('click',ta.animate.bind(ta));
+
+})
+
+
+
+class TextAnimation{
+    constructor(el){
+        
+        this.el =document.querySelector(el);
+        this.chars = this.el.innerHTML.trim().sprit("");
+        this.el.innerHTML = this._splitText();
+    }
+    _splitText(){
+        return this.chars.reduce((accu,curr) => {
+            curr = curr.replace(/\s+/,'&nbsp;');
+            return `${acc}<span class="char">${curr}</span>`;
+        },"");
+    }
+    animate(){
+        window.setTimeout(function(){
+            console.log(this);
+            this.el.classList.toggle('inview');
+        },bind(this));
+}
+
+   
+}
